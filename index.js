2c756a90173fd098997eb702c77943dd04c6bf4b
ztfcode <tkftronics@gmail.com>
2021-08-09 02:45:33 +0700

    second api

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.get("/user", (req, res) => {
+  let name = req.query.name
+  let id = req.query.id
+  if (name == null || name == '') name = null
+  if (id == null || id == '') id = null
+  let upx = {
+     "name": name,
+     "id": id*1,
+     "role": id*10-2+20-19,
+     "sha": '1aks8w9a0Jan8ak'
+  }
+  res.json(upx);
+});
+
+app.listen(port, () => {
+  console.log(`api-x listening at http://localhost:${port}`)
+});
