86ee5a9914a13af598a09b9f9860872ec35b5223
ztfcode <tkftronics@gmail.com>
2021-08-09 14:49:06 +0700

    seconds commits

--- index.js
+++ index.js
@@ -5,15 +5,30 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.get("/user", (req, res) => {
+  let api_key = req.query.api_key
+  const apikey = 'sagiri'
   let name = req.query.name
-  let id = req.query.id
-  if (name == null || name == '') name = null
-  if (id == null || id == '') id = null
+  let id = req.query.id * 1
+  let status = 'ok'
+  let msg = 'Valid apikey!'
+  if (api_key == apikey) {
+     if (name == null || name == '') name = null
+     if (id == null || id == '') id = null
+  } else {
+     [name, id] = [null, null]
+     status = 'bad'
+     msg = 'Invalid apikey!'
+  }
+  let err = {
+     "status": status,
+     "message": msg
+  }
   let upx = {
      "name": name,
-     "id": id*1,
+     "id": id,
      "role": id*10-2+20-19,
-     "sha": '1aks8w9a0Jan8ak'
+     "integrity": '1aks8w9a0Jan8ak',
+     "response": err
   }
   res.json(upx);
 });
